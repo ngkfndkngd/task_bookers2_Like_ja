e080285cac008c2072e8db7725dcd1977dfc8373
ngkfndkngd <huku.0033@gmail.com>
2021-10-07 11:07:11 +0000

    first commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -12,7 +12,10 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    
+    @book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+		redirect_back(fallback_location: root_path)
   end
   
   def book_comment_params
