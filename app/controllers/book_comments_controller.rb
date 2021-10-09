e1252b0af1663660f2115f76bf19cd215ca47aac
ngkfndkngd <huku.0033@gmail.com>
2021-10-09 07:14:42 +0000

    Finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,9 +5,7 @@ class BookCommentsController < ApplicationController
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
     if @book_comment.save
-      redirect_to book_path(@book.id)
-    else
-      render 'books/show'
+     
     end
   end
 
@@ -15,10 +13,10 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
   	book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_back(fallback_location: root_path)
+		
   end
   
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment,)
   end
 end
